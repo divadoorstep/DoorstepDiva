ac525fa475ab0e07e2748d433f43b427d8cd5393
divadoorstep <divadoorstep@gmail.com>
2025-07-29 12:31:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-https://github.com/divadoorstep/DoorstepDiva/blob/main/vite.config.ts// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  // Add this line:
-  base: '/',
 });
